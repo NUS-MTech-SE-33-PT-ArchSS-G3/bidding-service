a9504c15065198bfdde2938bef1042b192aaae92
Arabasta <keiyamx@gmail.com>
2025-09-30 01:19:26 +0800

    feat: infra layer add db

---
+++ internal/bidding/infrastructure/db/tx/tx.go
@@ -0,0 +1,39 @@
+package tx
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Manager struct{ DB *sql.DB }
+
+type txKey struct{}
+
+// WithinTx executes the given function within a db transaction
+func (m Manager) WithinTx(ctx context.Context, fn func(ctx context.Context) error) error {
+	if _, ok := ctx.Value(txKey{}).(*sql.Tx); ok {
+		// context already has a transaction, so just call the function
+		return fn(ctx)
+	}
+
+	tx, err := m.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	// inject tx into context
+	ctxWithTx := context.WithValue(ctx, txKey{}, tx)
+	if err := fn(ctxWithTx); err != nil {
+		// if error, rollback
+		_ = tx.Rollback()
+		return err
+	}
+
+	return tx.Commit()
+}
+
+// FromCtx extracts the *sql.Tx from context
+func FromCtx(ctx context.Context) (*sql.Tx, bool) {
+	tx, ok := ctx.Value(txKey{}).(*sql.Tx)
+	return tx, ok
+}
