e38f507949ea164c1ceb5269cfbc10af928f85ed
Arabasta <keiyamx@gmail.com>
2025-09-30 01:25:03 +0800

    feat: add kafka bid place event publisher

---
+++ internal/bidding/infrastructure/mq/bids_pub.go
@@ -0,0 +1,51 @@
+package mq
+
+import (
+	"context"
+	"encoding/json"
+	"kei-services/internal/bidding/application"
+	"kei-services/internal/bidding/domain"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+	"go.uber.org/zap"
+)
+
+var _ application.BidsPlacedPublisher = (*BidsPublisher)(nil)
+
+type BidsPublisher struct {
+	Writer *kafka.Writer
+	Topic  string
+	Log    *zap.Logger
+}
+
+func NewBidsPublisher(w *kafka.Writer, topic string, log *zap.Logger) BidsPublisher {
+	return BidsPublisher{
+		Writer: w,
+		Topic:  topic,
+		Log:    log,
+	}
+}
+
+func (p BidsPublisher) Publish(ctx context.Context, evt domain.BidPlaced) error {
+	payload, err := json.Marshal(evt)
+	if err != nil {
+		return err
+	}
+
+	msg := kafka.Message{
+		Key:   []byte(evt.AuctionID),
+		Value: payload,
+		Headers: []kafka.Header{
+			{Key: "content-type", Value: []byte("application/json")},
+			{Key: "schema", Value: []byte("bids.placed")},
+			{Key: "schema-version", Value: []byte("1")},
+		},
+		Topic: p.Topic,
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
+	defer cancel()
+
+	return p.Writer.WriteMessages(ctx, msg)
+}
---
+++ pkg/infra/kafka/writer.go
@@ -0,0 +1,55 @@
+package kafka
+
+// https://github.com/segmentio/kafka-go
+import (
+	"crypto/tls"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type WriterConfig struct {
+	Brokers     []string
+	Topic       string
+	ClientID    string
+	Acks        kafka.RequiredAcks // eg kafka.RequireAll
+	Compression kafka.Compression  // eg kafka.Snappy
+	SASLPlain   *struct{ Username, Password string }
+	TLS         *tls.Config
+	Balancer    kafka.Balancer // eg &kafka.Hash{}, &kafka.LeastBytes{}
+}
+
+func NewWriter(cfg WriterConfig) *kafka.Writer {
+	w := &kafka.Writer{
+		Addr:         kafka.TCP(cfg.Brokers...),
+		Topic:        cfg.Topic,
+		Balancer:     firstNonNil(cfg.Balancer, &kafka.Hash{}), // hash by Key
+		RequiredAcks: firstAcks(cfg.Acks, kafka.RequireAll),
+		Compression:  firstCompression(cfg.Compression, kafka.Snappy),
+		BatchTimeout: 20 * time.Millisecond,
+		BatchBytes:   64 << 10, // 64KB
+		Async:        true,
+	}
+
+	// todo: add metrics hook
+	// todo: add logger hook
+	// todo WriterTransport tls
+
+	return w
+}
+
+func firstNonNil[T any](v, def T) T { return v }
+
+func firstAcks(v, def kafka.RequiredAcks) kafka.RequiredAcks {
+	if v == 0 {
+		return def
+	}
+	return v
+}
+
+func firstCompression(v, def kafka.Compression) kafka.Compression {
+	if v == 0 {
+		return def
+	}
+	return v
+}
