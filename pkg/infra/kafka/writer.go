17ec31c6a76e6cfdbd3a5a03999a8308ec6fc6cd
Arabasta <keiyamx@gmail.com>
2025-10-15 16:12:40 +0800

    feat: wire zap logger into segmentio kafka writer

--- pkg/infra/kafka/writer.go
+++ pkg/infra/kafka/writer.go
@@ -3,9 +3,11 @@ package kafka
 // https://github.com/segmentio/kafka-go
 import (
 	"crypto/tls"
+	"fmt"
 	"time"
 
 	"github.com/segmentio/kafka-go"
+	"go.uber.org/zap"
 )
 
 type WriterConfig struct {
@@ -19,7 +21,7 @@ type WriterConfig struct {
 	Balancer    kafka.Balancer // eg &kafka.Hash{}, &kafka.LeastBytes{}
 }
 
-func NewWriter(cfg WriterConfig) *kafka.Writer {
+func NewWriter(cfg WriterConfig, log *zap.Logger) *kafka.Writer {
 	w := &kafka.Writer{
 		Addr:         kafka.TCP(cfg.Brokers...),
 		Topic:        cfg.Topic,
@@ -30,10 +32,19 @@ func NewWriter(cfg WriterConfig) *kafka.Writer {
 		BatchBytes:   64 << 10, // 64KB
 		Async:        true,
 	}
+	w.AllowAutoTopicCreation = true
 
 	// todo: add metrics hook
-	// todo: add logger hook
+
+	w.Logger = kafka.LoggerFunc(func(msg string, args ...interface{}) {
+		log.Debug("kafka writer", zap.String("msg", fmt.Sprintf(msg, args...)))
+	})
+	w.ErrorLogger = kafka.LoggerFunc(func(msg string, args ...interface{}) {
+		log.Error("kafka writer error", zap.String("msg", fmt.Sprintf(msg, args...)))
+	})
+
 	// todo WriterTransport tls
+	//w.Addr = kafka.TCP(cfg.Brokers...)
 
 	return w
 }
